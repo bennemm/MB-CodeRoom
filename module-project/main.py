f96dbf9cdf6f09b375d34f5f83ce3de67288b178
bennemm <50249293+bennemm@users.noreply.github.com>
2019-05-07 20:19:07 -0600

    Update main.py

--- module-project/main.py
+++ module-project/main.py
@@ -5,7 +5,7 @@ Created on Fri May  3 11:52:16 2019
 @author: benne
 """
 
-#from list_math import*   
+from list_math import*   
 list1 = list(map(int, 
 input("Please enter a list of numbers, separated by a space:")
 .split()))
