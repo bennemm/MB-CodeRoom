a468e0f912dbcd38194dbd6cb1832d0bb6037513
bennemm <50249293+bennemm@users.noreply.github.com>
2019-05-07 20:41:55 -0600

    Add files via upload

---
+++ Import_NumPy.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  5 12:40:19 2019
+
+@author: benne
+"""
+""" Learning to import the NumPy Library """
+
+import numpy as np
+
+x = np.array([1,2,3])
+
+print (sum(x))
